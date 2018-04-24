e236a16fa7eea62894ee7c6c7a0d18fbe7a14d2b
mbuffery <miles.buffery@students.plymouth.ac.uk>
2018-04-24 04:34:11 +0100

    App now saves user data onto realtime database
    CAn save basic information of the user e.g. height and age etc. Will move more onto getting more advnaced information saved next.

--- app/src/main/java/com/example/miles/fitness4u/ProfileFragment.java
+++ app/src/main/java/com/example/miles/fitness4u/ProfileFragment.java
@@ -13,6 +13,11 @@ import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by miles on 4/16/2018.
@@ -25,7 +30,7 @@ public class ProfileFragment extends Fragment {
             , WeightReg, HeightReg, EmailReg, PasswordReg;
 
     private Button RegButton;
-
+    private DatabaseReference as;
     private FirebaseAuth auth;
 
 
@@ -42,6 +47,8 @@ public class ProfileFragment extends Fragment {
         //Gets the firebase Authenticator
         auth = FirebaseAuth.getInstance();
 
+
+        //Sets each edit text to a variable
         NameReg = (EditText) getView().findViewById(R.id.nameReg);
         AgeReg = (EditText) getView().findViewById(R.id.ageReg);
         SexReg = (EditText) getView().findViewById(R.id.sexReg);
@@ -56,13 +63,36 @@ public class ProfileFragment extends Fragment {
         //
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         //Adds listener to the button
+
+        //When button is pressed, it saves all the data onto the database, e.g. Sex
         RegButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (v.equals(RegButton))
                 {
                     String user_id = auth.getCurrentUser().getUid();
-                    DatabaseReference current
+                    DatabaseReference curren_userdb = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+
+                    String name = NameReg.getText().toString();
+                    String age = AgeReg.getText().toString();
+                    String sex = SexReg.getText().toString();
+                    String weight = WeightReg.getText().toString();
+                    String height = HeightReg.getText().toString();
+                    String email = EmailReg.getText().toString();
+                    String password = PasswordReg.getText().toString();
+
+                    Map newPost = new HashMap();
+                    newPost.put("Name", name);
+                    newPost.put("Age", age);
+                    newPost.put("Sex", sex);
+                    newPost.put("Weight", weight);
+                    newPost.put("Height", height);
+                    newPost.put("Email", email);
+                    newPost.put("Password", password);
+
+                    curren_userdb.setValue(newPost);
+
+
                 }
 
             }
