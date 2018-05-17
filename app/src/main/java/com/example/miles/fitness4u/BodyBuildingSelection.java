be2815b1212e264fbb846f038160534360d34a63
mbuffery <miles.buffery@students.plymouth.ac.uk>
2018-05-17 02:16:47 +0100

    Create a data model for the activity

---
+++ app/src/main/java/com/example/miles/fitness4u/BodyBuildingSelection.java
@@ -0,0 +1,19 @@
+package com.example.miles.fitness4u;
+
+/**
+ * Created by miles on 5/17/2018.
+ */
+
+public class BodyBuildingSelection {
+
+    public String title;
+    public String mainText;
+    public int imageId;
+
+    public BodyBuildingSelection(String title, String mainText, int imageId)
+    {
+        this.title = title;
+        this.mainText = mainText;
+        this.imageId = imageId;
+    }
+}
--- app/src/main/java/com/example/miles/fitness4u/Bodybuilding_abs/CrunchesFragment.java
+++ app/src/main/java/com/example/miles/fitness4u/Bodybuilding_abs/CrunchesFragment.java
@@ -1,6 +1,8 @@
 package com.example.miles.fitness4u.Bodybuilding_abs;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,28 +13,21 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.miles.fitness4u.BodyBuildingSelection;
 import com.example.miles.fitness4u.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by miles on 5/16/2018.
  */
 
-public class CrunchesFragment extends Fragment {
+public class CrunchesFragment extends ArrayAdapter<BodyBuildingSelection> {
+    Context context;
+    int resource;
+    ArrayList<BodyBuildingSelection> selection = null;
 
-    ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.bodybuilding_abs, container, false);
-    }
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-
-
-
-
+    public CrunchesFragment(@NonNull Context context, int resource, ArrayList<BodyBuildingSelection> selection) {
+        super(context, resource);
     }
 }
