b3316dd74550812c3d7159139834e2dcb83c36af
mbuffery <miles.buffery@students.plymouth.ac.uk>
2018-05-19 14:25:12 +0100

    Started to implment a acurate step counter. Added an algorithm to filter out all of the sensor vaules and only get the best

--- app/src/main/java/com/example/miles/fitness4u/FitnessFragment.java
+++ app/src/main/java/com/example/miles/fitness4u/FitnessFragment.java
@@ -9,6 +9,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,21 +17,26 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static android.content.Context.SENSOR_SERVICE;
+
 /**
  * Created by miles on 4/16/2018.
  */
 
-public class FitnessFragment extends Fragment implements SensorEventListener{
+public class FitnessFragment extends Fragment implements SensorEventListener, StepListener{
 
 
-    private long steps = 0;
+    //private long steps = 0;
     TextView tvSteps;
-    Sensor countSensor;
-    SensorManager sManager;
-    boolean running = false;
+    //Sensor countSensor;
+    //SensorManager sManager;
+    //boolean running = false;
     Button startCounter, endCounter;
-
-
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private static final String TEXT_NUM_STEPS = "Number of Steps: ";
+    private int numSteps;
 
 
     @Nullable
@@ -43,42 +49,49 @@ public class FitnessFragment extends Fragment implements SensorEventListener{
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        
         tvSteps = getView().findViewById(R.id.tvSteps);
         startCounter = getView().findViewById(R.id.startCounter);
         endCounter = getView().findViewById(R.id.stopCounter);
 
         //Initialised SensorManager and Sensor
-        sManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        //sManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+
 
 
 
         startCounter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v.equals(startCounter)) {
+                //if (v.equals(startCounter)) {
 
-                    startCounter();
+                   // startCounter();
 
-                }
+               // }
+                numSteps = 0;
+                sensorManager.registerListener(FitnessFragment.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+                Toast.makeText(getActivity(), "Start Walking", Toast.LENGTH_SHORT).show();
             }
         });
 
         endCounter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v.equals(startCounter)) {
+                //if (v.equals(startCounter)) {
 
-                    stopCounter();
+                   // stopCounter();
 
                     //tvSteps.setText("");
-                }
+               // }
+                sensorManager.unregisterListener(FitnessFragment.this);
+                Toast.makeText(getActivity(), "Stop Walking", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
+   // @Override
+   // public void onSensorChanged(SensorEvent event) {
+/*
         if (running)
         {
             tvSteps.setText(String.valueOf(event.values[0]));
@@ -88,7 +101,7 @@ public class FitnessFragment extends Fragment implements SensorEventListener{
         //TODO: Restart step counter everytime you press go or stop.
         //TODO: After getting the amount of steps, calculate into distance travelled
 
-/*
+
         Sensor sensor = event.sensor;
         float[] values = event.values;
         int value = -1;
@@ -100,7 +113,7 @@ public class FitnessFragment extends Fragment implements SensorEventListener{
         if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
             steps++;
         }*/
-    }
+    //}
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
@@ -109,7 +122,7 @@ public class FitnessFragment extends Fragment implements SensorEventListener{
     }
 
     public void startCounter()
-    {
+    {/*
         running = true;
         Sensor countSensor = sManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
@@ -125,12 +138,13 @@ public class FitnessFragment extends Fragment implements SensorEventListener{
         }
 
         //sManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        */
     }
 
 
     public void stopCounter() {
 
-        running = false;
+        //running = false;
         //sManager.unregisterListener(this);
     }
 
@@ -140,8 +154,14 @@ public class FitnessFragment extends Fragment implements SensorEventListener{
         return distance;
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
 
+    }
 
+    @Override
+    public void step(long timeNs) {
 
-
+    }
+   
 }
--- app/src/main/java/com/example/miles/fitness4u/SensorFilter.java
+++ app/src/main/java/com/example/miles/fitness4u/SensorFilter.java
@@ -5,5 +5,43 @@ package com.example.miles.fitness4u;
  */
 
 public class SensorFilter {
-    
+    private SensorFilter() {
+    }
+
+    //Algorithm to filter out the values, this will give an calculated estimate to steps
+
+    //There will be multiple values, filters out any values that are unwanted from the accelerometer data
+
+    public static float sumTotal(float[] array) {
+        float values = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            values += array[i];
+        }
+        return values;
+    }
+
+    //Crosses the vaules that has been recorded from the stepcounter
+    public static float[] crossValues(float[] array1, float[] array2) {
+
+        //Creates 3 arrays
+        float[] valuesArray = new float[3];
+
+        //Each array has now crossed their values and been set to valuesArrary
+        valuesArray[0] = array1[1] * array2[2] - array1[2] * array2[1];
+        valuesArray[1] = array1[2] * array2[0] - array1[0] * array2[2];
+        valuesArray[2] = array1[0] * array2[1] - array1[1] * array2[0];
+
+        return valuesArray;
+    }
+
+    public static float norm(float[] array) {
+        float values = 0;
+        for (int i = 0; i < array.length; i++) {
+            values += array[i] * array[i];
+        }
+        return (float) Math.sqrt(values);
+    }
+
+
 }
