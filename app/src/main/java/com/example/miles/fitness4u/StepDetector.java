19be0fb208968fdd1d695e169f029b560f1bd5f2
mbuffery <miles.buffery@students.plymouth.ac.uk>
2018-05-19 16:01:22 +0100

    Each variable created in order for each step to be detected

--- app/src/main/java/com/example/miles/fitness4u/StepDetector.java
+++ app/src/main/java/com/example/miles/fitness4u/StepDetector.java
@@ -10,7 +10,29 @@ public class StepDetector {
     private static final int accelRingSize = 50;
     private static final int velocityRingSize = 10;
 
-    
+    //Found it that 50 is a good sensitivity as it works on multiple update to date android phones
+    private static final float startStepSens = 50f;
+
+    //After each step, give a delay
+    private static final int stepDelay = 250000000;
+
+    private int accelRingCounter = 0;
+    private int velRingCounter = 0;
+    //Sets the X, Y, Z for the accelRing by assigning it a float
+    private float[] xAccelRing = new float[accelRingSize];
+    private float[] yAccelRing = new float[accelRingSize];
+    private float[] zAccelRing = new float[accelRingSize];
+
+    private float[] velRing = new float[velocityRingSize];
+
+    //Set lastStepTime to 0
+    private long lastTimeStepped = 0;
+    private float oldVelocityEstimate = 0;
+
+    //Uses the StepListener interface to listen and calculate each step
+    private StepListener listener;
+
+
 
 
 }
