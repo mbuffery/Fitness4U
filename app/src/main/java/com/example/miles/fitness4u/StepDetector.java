c2a5f4a1ba91f35ae3fe62d05014d6966e84fed6
mbuffery <miles.buffery@students.plymouth.ac.uk>
2018-05-19 15:42:40 +0100

    Created a step detector class and set the acceler and velocity variable

---
+++ app/src/main/java/com/example/miles/fitness4u/StepDetector.java
@@ -0,0 +1,16 @@
+package com.example.miles.fitness4u;
+
+/**
+ * Created by miles on 5/19/2018.
+ */
+
+public class StepDetector {
+
+    //Sets the accelerometer and velocity
+    private static final int accelRingSize = 50;
+    private static final int velocityRingSize = 10;
+
+    
+
+
+}
