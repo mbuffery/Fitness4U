9fae645c713f6cffa2b26257ac842c8c14bb10c9
mbuffery <miles.buffery@students.plymouth.ac.uk>
2018-05-19 13:00:54 +0100

    Going with a differnt way of creating a distance travelled

---
+++ app/src/main/java/com/example/miles/fitness4u/SensorFilter.java
@@ -0,0 +1,9 @@
+package com.example.miles.fitness4u;
+
+/**
+ * Created by miles on 5/19/2018.
+ */
+
+public class SensorFilter {
+    
+}
---
+++ app/src/main/java/com/example/miles/fitness4u/StepListener.java
@@ -0,0 +1,10 @@
+package com.example.miles.fitness4u;
+
+/**
+ * Created by miles on 5/19/2018.
+ */
+
+public interface StepListener {
+
+    public void step(long timeNs);
+}
